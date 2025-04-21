c5725f028dbfdd39b39735301ce69c7cd09341c8
Adarsha Shrestha <adarsha.shrestha@compliancequest.com>
2025-04-21 14:44:33 +0545

    [feature/1_0_0_desing] added a snakeHead at 5,5 and food at random location

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.Random;
 
 import javax.swing.*;
 
@@ -17,13 +18,23 @@ public class SnakeGame extends JPanel {
     int boardWidth;
     int boardHeight;
     Tile snakeHead;
+    Tile food;
+    Random random;
     int tileSize = 25; // Size of each tile in pixels
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight; 
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.BLACK);     
+        setBackground(Color.BLACK);
+
         snakeHead = new Tile(5,5); // Initialize snake head position
+        food = new Tile(10, 10);
+
+        random = new Random();
+        placeFood(random); // Place food at a random position
+
+
+    
     }
 
     public void paintComponent(Graphics g) {
@@ -32,10 +43,33 @@ public class SnakeGame extends JPanel {
     }
 
     public void draw(Graphics g) {
-        int pixelx = snakeHead.x * tileSize; // Convert tile coordinates to pixel coordinates
-        int pixely = snakeHead.y * tileSize; // Convert tile coordinates to pixel coordinates
+        for(int i =0;i<boardWidth/tileSize;i++){
+            //(x1,y1,x2,y2)
+            // Draw vertical and horizontal lines to create a grid
+            g.drawLine(i*tileSize,0,i*tileSize, boardWidth);
+            g.drawLine(0,i*tileSize,boardWidth,i*tileSize);
+            for(i=0;i<boardHeight/tileSize;i++){
+                g.drawLine(i*tileSize,0,i*tileSize, boardHeight);
+                g.drawLine(0,i*tileSize,boardHeight,i*tileSize);
+            }
+        }
+
+        //draw snake head
         g.setColor(Color.GREEN);
-        g.fillRect(pixelx, pixely,tileSize, tileSize); // Draw snake head
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize,tileSize, tileSize); // Draw snake head
+
+        //draw food
+        g.setColor(Color.RED);
+        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize); // Draw food at (10, 10) for now
+    }
+
+    public void placeFood(Random random) {
+        // Generate random coordinates for food
+        int x = random.nextInt(boardWidth / tileSize); //600/25 = 24 => 0-23
+        int y = random.nextInt(boardHeight / tileSize); //600/25 = 24 => 0-23
+        food.x = x;
+        food.y = y;
+    
     }
     
 }
