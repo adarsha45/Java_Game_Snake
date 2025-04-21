2f840f4b88ca628de569a3f6bf007e229b3c9287
Adarsha Shrestha <adarsha.shrestha@compliancequest.com>
2025-04-21 15:54:17 +0545

    [feature/1_0_0_design] implemented the action listener and keylistener for moving and listening to key actions

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,10 +1,10 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.util.Random;
 
 import javax.swing.*;
 
-public class SnakeGame extends JPanel {
-
+public class SnakeGame extends JPanel implements ActionListener,KeyListener {
     private class Tile {
         int x;
         int y;
@@ -14,18 +14,26 @@ public class SnakeGame extends JPanel {
             this.y = y;
         }
     }
-
+    int tileSize = 25; // Size of each tile in pixels
     int boardWidth;
     int boardHeight;
+    //snake
     Tile snakeHead;
+    //food
     Tile food;
     Random random;
-    int tileSize = 25; // Size of each tile in pixels
+    //game logic
+    Timer gameLoop;
+    int velocityX; // Speed of the snake
+    int velocityY; // Speed of the snake
+
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight; 
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
+        addKeyListener(this); // Add key listener to the panel
+        setFocusable(true); // Make the panel focusable
 
         snakeHead = new Tile(5,5); // Initialize snake head position
         food = new Tile(10, 10);
@@ -33,7 +41,11 @@ public class SnakeGame extends JPanel {
         random = new Random();
         placeFood(random); // Place food at a random position
 
+        velocityX = 0; // Set initial speed of the snake
+        velocityY = 0; // Set initial speed of the snake
 
+        gameLoop = new Timer(100, this); // Timer to control the game loop
+        gameLoop.start();
     
     }
 
@@ -70,6 +82,52 @@ public class SnakeGame extends JPanel {
         food.x = x;
         food.y = y;
     
+    }
+
+    public void move(){
+        snakeHead.x += velocityX; // Move snake head to the right by one tile
+        snakeHead.y += velocityY; // Keep snake head in the same row
+        if(snakeHead.y >= boardHeight/tileSize){
+            snakeHead.y = 0; // Wrap around to the top of the board
+        }else if(snakeHead.y < 0){
+            snakeHead.y = boardHeight/tileSize - 1; // Wrap around to the bottom of the board
+        }else if(snakeHead.x >= boardWidth/tileSize){
+            snakeHead.x = 0; // Wrap around to the left of the board
+        }else if(snakeHead.x < 0){
+            snakeHead.x = boardWidth/tileSize - 1; // Wrap around to the right of the board
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // change the position of the snake head
+        move();
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP) {
+            velocityX = 0; // Move snake head up
+            velocityY = -1; // Move snake head up
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            velocityX = 0; // Move snake head down
+            velocityY = 1; // Move snake head down
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            velocityX = -1; // Move snake head left
+            velocityY = 0; // Move snake head left
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            velocityX = 1; // Move snake head right
+            velocityY = 0; // Move snake head right
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
     }
     
 }
